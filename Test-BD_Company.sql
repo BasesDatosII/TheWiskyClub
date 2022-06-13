5b94551610e62283e5963347c648cbf6a502f524
Esteban <estebanlm@estudiantec.cr>
2022-06-12 21:42:10 -0600

    Update Procedures Company

--- Test-BD_Company.sql
+++ Test-BD_Company.sql
@@ -10,4 +10,4 @@ INSERT INTO Country (idCountry, countryName ) VALUES(353, 'Ireland')
 
 --Inserts for departments's
 INSERT INTO Department(departmentName)VALUES ('Administration')
-INSERT INTO Job (jobName, idDepartment) VALUES ('Administrator',1) -- job from adminiistrator
+INSERT INTO Job (jobName, idDepartment) VALUES ('Administrator',1) -- job for administrator

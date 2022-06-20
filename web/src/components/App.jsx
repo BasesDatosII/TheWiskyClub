54fad0f933fe0c1af10d0a2890d0c16d7e6dd5c9
Esteban <estebanlm@estudiantec.cr>
2022-06-20 03:07:14 -0600

    Auto stash before merge of "OSMImplement" and "origin/OSMImplement"

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -2,16 +2,22 @@ import React from "react";
 
 //version 1.0
 
-import Home from './Home'
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+
 import MapAPI from "./MapAPI";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 export function App(){
-    return (
-    <React.Fragment>
-
-      <MapAPI></MapAPI>
-    </React.Fragment>
-
+    return (root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+          <React.Fragment>
+            <MapAPI></MapAPI>
+          </React.Fragment>
+        </BrowserRouter>
+    </React.StrictMode>)
     );
 }

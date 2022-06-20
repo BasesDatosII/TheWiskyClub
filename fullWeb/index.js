a653957c91ad1a35be2edbf54d0ad54854fddd8c
Esteban <estebanlm@estudiantec.cr>
2022-06-20 09:11:56 -0600

    setRender does not work

--- fullWeb/index.js
+++ fullWeb/index.js
@@ -8,7 +8,7 @@ Esteban Leiva Montenegro
 */ 
 
 const viewsDirectory = 'views'; 
-const srcDirectory  = '/src'
+const srcDirectory  = '/src';
 
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
@@ -26,10 +26,7 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
     //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
 });
 
-app.get('/map.html', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile(srcDirectory.concat('/map.html'), {root: __dirname});      //server responds by sending the index.html file to the client's browser
-    //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
-});
+
 
 app.get('/locations', (req, res) => {        //get requests to the root ("/") will route here
     res.json({
@@ -39,10 +36,13 @@ app.get('/locations', (req, res) => {        //get requests to the root ("/") wi
 });
 
 
+/*-------------------------------------General upload files--------------------------------------------------*/ 
 
 app.get('/Resource/:dir/:name', async function(req, res){
    let resource = (req.params.dir).concat('/').concat(req.params.name);
-   res.sendFile(resource,{root: __dirname})
+    
+    //res.render('Your-Account.ejs',{people});
+    res.sendFile(resource,{root: __dirname})
 });
 app.get('/Resource/:dirA/:dirB/:name', async function(req, res){
    let resource = (req.params.dirA).concat('/').concat(req.params.dirB).concat('/').concat(req.params.name);
@@ -60,38 +60,12 @@ app.get('/Resource/:name', async function(req, res){
     res.sendFile(resource,{root: __dirname})
 });
 
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Starting development-> Now listening port ${port}`);
-});
-
-/*-------------------------------------General upload files--------------------------------------------------*/ 
-
-app.get('/Resource/:dir/:name', async function(req, res){
-   let resource = (req.params.dir).concat('/').concat(req.params.name);
-   
-});
-app.get('/Resource/:dirA/:dirB/:name', async function(req, res){
-   let resource = (req.params.dirA).concat('/').concat(req.params.dirB).concat('/').concat(req.params.name);
-   res.sendFile(resource,{root: __dirname})
-});
-app.get('/Resource/:dirA/:dirB/:dirC/:name', async function(req, res){
-   let resource = (req.params.dirA).concat('/')
-   .concat(req.params.dirB).concat('/')
-   .concat(req.params.dirC).concat('/')
-   .concat(req.params.name);
-   res.sendFile(resource,{root: __dirname})
-});
-
-
 app.listen(port, () => {   //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Starting development-> Now listening port ${port}`);
+    console.log(`Starting...-> Now listening port ${port}`);
 });
 
 //-------------------------------------RENDER --------------------------------------------------*/ 
-app.get('/Resource/src/:name', async function(req, res){
-    let resource = (req.params.name);
-    res.sendFile(resource,{root: __dirname})
-});
+
 
  
 //-------------------------------------LOG IN VIEW --------------------------------------------------*/ 
@@ -103,9 +77,16 @@ app.post('/dLogIn', urlencodeParser, function(req, res){
 
 //-------------------------------------Your-account view --------------------------------------------------*/ 
 
-app.post('/injectYour-AccountData', urlencodeParser, function(req,res){
-    res.render('Your-Account',{idPerson: 'Esteban'});
+app.get('/Resource/:dir/:name', urlencodeParser, function(req,res){
+    console.log('using ejs');
+    if(req.params.name.localeCompare('Your-Account.html')){
+        console.log('Render person into acount');
+        var people = {idPerson: '', name: 'Esteban' };
+        res.render('Your-Account.ejs',{people});
+    }
+    
 });
+
 
 //-------------------------------------Register view  --------------------------------------------------*/ 
 

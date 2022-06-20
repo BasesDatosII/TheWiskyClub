d2ff8f6bfa8c0e154a651b92610526b275c6f7c9
Esteban <estebanlm@estudiantec.cr>
2022-06-20 07:22:27 -0600

    update template

--- fullWeb/index.js
+++ fullWeb/index.js
@@ -1,11 +1,22 @@
 
 
+/*
+Created by:
+Daniel Araya Sambucci
+Ana Guevara
+Esteban Leiva Montenegro
+*/ 
+
 const viewsDirectory = 'views'; 
 const srcDirectory  = '/src'
 
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5000;                  //Save the port number where your server will be listening
+
+
+const bodyParser = require('body-parser');
+const urlencodeParser = bodyParser.urlencoded({extended: false});
 
 app.set('view engine', 'ejs');
 
@@ -53,15 +64,49 @@ app.listen(port, () => {            //server starts listening for any attempts f
     console.log(`Starting development-> Now listening port ${port}`);
 });
 
+/*-------------------------------------General upload files--------------------------------------------------*/ 
 
-/*document.getElementById("submitButton").addEventListener("click", function(){
-    console.log(`Selecten item!`);
+app.get('/Resource/:dir/:name', async function(req, res){
+   let resource = (req.params.dir).concat('/').concat(req.params.name);
+   
+});
+app.get('/Resource/:dirA/:dirB/:name', async function(req, res){
+   let resource = (req.params.dirA).concat('/').concat(req.params.dirB).concat('/').concat(req.params.name);
+   res.sendFile(resource,{root: __dirname})
+});
+app.get('/Resource/:dirA/:dirB/:dirC/:name', async function(req, res){
+   let resource = (req.params.dirA).concat('/')
+   .concat(req.params.dirB).concat('/')
+   .concat(req.params.dirC).concat('/')
+   .concat(req.params.name);
+   res.sendFile(resource,{root: __dirname})
+});
 
-    var json = JSON.stringify({
-        id: parseInt(this.typeId),
-        subject: this.datatype,
-        points: parseInt(this.points),
-        user: "H. Pauwelyn"
-    });
-    console.log(`Selecten item!` + json);
-});*/
+
+app.listen(port, () => {   //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Starting development-> Now listening port ${port}`);
+});
+
+//-------------------------------------RENDER --------------------------------------------------*/ 
+app.get('/Resource/src/:name', async function(req, res){
+    let resource = (req.params.name);
+    res.sendFile(resource,{root: __dirname})
+});
+
+ 
+//-------------------------------------LOG IN VIEW --------------------------------------------------*/ 
+app.post('/dLogIn', urlencodeParser, function(req, res){
+    console.log(req.body);
+});
+
+//-------------------------------------Collection view --------------------------------------------------*/ 
+
+//-------------------------------------Your-account view --------------------------------------------------*/ 
+
+app.post('/injectYour-AccountData', urlencodeParser, function(req,res){
+    res.render('Your-Account',{idPerson: 'Esteban'});
+});
+
+//-------------------------------------Register view  --------------------------------------------------*/ 
+
+//-------------------------------------Cart view  --------------------------------------------------*/ 
